b12ca26454c19ed4902b479e550cc264dbb797b5
Celeste <cecuercu@gmail.com>
2020-11-12 17:42:02 +0100

    Clase 2 de JavaFX - Nombre y edad Perro

---
+++ src/sample/Controller.java
@@ -0,0 +1,27 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class Controller {
+    Perro perro = new Perro();
+
+    String nombre = "";
+
+    @FXML
+    private Label idlabelEdad;
+    @FXML
+    private Label idlabelNombre;
+
+    @FXML
+    public void onEdadClicked(MouseEvent event) {
+        idlabelEdad.setText(perro.edad.toString());
+    }
+
+    @FXML
+    public void onNombreClicked() {
+        nombre += perro.nombre;
+        idlabelNombre.setText(nombre);
+    }
+}
---
+++ src/sample/Perro.java
@@ -0,0 +1,6 @@
+package sample;
+
+public class Perro {
+    String nombre = "Néstor";
+    Integer edad = 3;
+}
